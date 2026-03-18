18d479b82d8c5446a4445921f50341149769c041
bizdikian <arthur@lemonadefashion.com>
2026-03-17 19:25:15 -0700

    Initial setup for the main application file (pages/_app.js).

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+// This is the main application file
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
